63e97aad175782791f74e82f95a339757bf62049
Corey Olson <me@olscore.com>
2016-01-28 11:17:01 -0600

    Skeleton class helpers\text -> helpers\str

---
+++ application/helpers/str.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Ornithopter.io
+ * ------------------------------------------------
+ * A minimalist, high-speed open source PHP 5.6+ framework
+ *
+ * @package     Ornithopter.io
+ * @author      Corey Olson
+ * @copyright   Copyright (c) 2011 - 2016 Corey Olson
+ * @license     http://opensource.org/licenses/MIT (MIT License)
+ * @link        https://github.com/olscore/ornithopter.io
+ * @version     2016.01.20
+ */
+
+ // ########################################################################################
+
+/**
+ * A helper class for assisting with text and string manipulation
+ *
+ * @author      Corey Olson
+ * @package     Ornithopter.io
+ * @subpackage	Helpers
+ *
+ * @method
+ */
+namespace helpers;
+class str
+{
+	/**
+	 * Allows global self reference
+	 *
+	 * @var array
+	 */
+	public static $self;
+
+	/**
+	 * Initialize str helper class
+	 *
+	 * @return  object
+	 */
+    public function __construct()
+    {
+		// Create an instance
+		self::$self = $this;
+
+		// Register shortcut aliases using h::method();
+		\io::alias('helpers\str', get_class_methods(__CLASS__));
+	}
+
+	/**
+	 * Creates a shortcut for io::arr()
+	 *
+	 * @return  object
+	 */
+	public static function str()
+	{
+		// Shortcut for io::str()
+		return self::$self;
+	}
+
+	// TODO: Make io::helper('str'); class
+
+	/**
+	 * Method aliases and function wrappers for coders who like to use alternative
+	 * names for these methods. Slight performance impact when using method aliases.
+	 *
+	 * @param   string
+	 * @param   mixed
+	 * @return  mixed
+	 */
+	public static function __call( $called, $args = array() )
+	{
+		$aliases = array(
+			'name'		=> ['alias']
+		);
+
+		// Iterate through methods
+		foreach ( $aliases as $method => $list )
+
+			// Check called against accepted alias list
+			if ( in_array($called, $list) )
+
+				// Dynamic method (alias) call with arbitrary arguments
+				return call_user_func_array(array(__CLASS__, $method), $args);
+
+		// No alias found
+		return false;
+	}
+}
