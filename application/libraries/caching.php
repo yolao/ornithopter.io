465f2c4ea39735834183b436585d8be917057df4
Corey Olson <me@olscore.com>
2016-02-14 15:51:34 -0600

    Fixed a broken class

--- application/libraries/caching.php
+++ application/libraries/caching.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2011 - 2016 Corey Olson
  * @license     http://opensource.org/licenses/MIT (MIT License)
  * @link        https://github.com/olscore/ornithopter.io
- * @version     2016.02.05
+ * @version     2016.02.14
  */
 
  // ########################################################################################
@@ -67,15 +67,42 @@ class caching
 		// Register shortcut aliases using io::method();
 		\io::alias('libraries\caching', get_class_methods(__CLASS__));
 	}
-}
 
-/**
- * Creates a shortcut for io::caching()
- *
- * @return  object
- */
-public static function caching()
-{
-	// Shortcut for io::caching()
-	return self::$instance;
+	/**
+	 * Creates a shortcut for io::caching()
+	 *
+	 * @return  object
+	 */
+	public static function caching()
+	{
+		// Shortcut for io::caching()
+		return self::$instance;
+	}
+
+	/**
+	 * Method aliases and function wrappers for coders who like to use alternative
+	 * names for these methods. Slight performance impact when using method aliases.
+	 *
+	 * @param   string
+	 * @param   mixed
+	 * @return  mixed
+	 */
+	public function __call( $called, $args = array() )
+	{
+		$aliases = array(
+			'method' => ['shortcut']
+		);
+
+		// Iterate through methods
+		foreach ( $aliases as $method => $list )
+
+			// Check called against accepted alias list
+			if ( in_array($called, $list) )
+
+				// Dynamic method (alias) call with arbitrary arguments
+				return call_user_func_array(array(__CLASS__, $method), $args);
+
+		// No alias found
+		return false;
+	}
 }
