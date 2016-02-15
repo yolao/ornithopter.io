99531ecd395d9edfa26c0be4d23b350247d8a1d4
Corey Olson <me@olscore.com>
2016-02-14 21:25:37 -0600

    Significant refactor, added methods, detailed info

--- application/libraries/agent.php
+++ application/libraries/agent.php
@@ -29,7 +29,14 @@
  * @method 		io::library('agent')->agent_site();
  * @method 		io::library('agent')->agent_custom();
  * @method 		io::library('agent')->execute();
+ * @method 		io::library('agent')->status();
+ * @method 		io::library('agent')->path();
+ * @method 		io::library('agent')->protocol();
+ * @method 		io::library('agent')->domain();
+ * @method 		io::library('agent')->headers();
+ * @method 		io::library('agent')->body();
  * @method 		io::library('agent')->redirects();
+ * @method 		io::library('agent')->details();
  * @method 		io::library('agent')->get();
  * @method 		io::library('agent')->post();
  * @method 		io::library('agent')->put();
@@ -44,6 +51,25 @@ class agent
 	 * @var array
 	 */
 	private $data;
+
+	/**
+	 * Ignore duplicate CURL information
+	 *
+	 * @var array
+	 */
+	private static $ignore = array('url', 'content_type', 'http_code',
+		'ssl_verify_result', 'redirect_count', 'redirect_url', 'certinfo');
+
+	/**
+	 * Internal class parameters for resetting
+	 *
+	 * @var array
+	 */
+	private $param = array(
+		'pre'    => ['code', 'headers', 'protocol', 'domain', 'root', 'body', 'details'],
+		'post'   => ['post', 'put', 'delete'],
+		'status' => ['status', 'redirect']
+	);
 
 	/**
 	 * Initialize the agent class
@@ -62,17 +88,8 @@ class agent
 		// Configure user agent (site or domain)
 		$this->data['agent']['site'] = \io::helper('web')->site();
 
-		// Initialize NULL paramters
-		foreach (['headers', 'body', 'post', 'put', 'delete'] as $key)
-
-			// Initialize with NULL data
-			$this->data[$key] = null;
-
-		// Initialize FALSE paramters
-		foreach (['status', 'redirect'] as $key)
-
-			// Initialize with FALSE data
-			$this->data[$key] = null;
+		// Prepares parameters for new CURL request
+		$this->resetter('pre', 'post', 'status');
 
 		// Register shortcut aliases using io::method();
 		\io::alias('libraries\agent', get_class_methods(__CLASS__));
@@ -162,7 +179,7 @@ class agent
 	}
 
 	/**
-	 * Request Execution
+	 * Main Request Execution
 	 *
 	 * @param 	mixed
 	 * @return 	mixed
@@ -176,6 +193,9 @@ class agent
 			return false;
 		}
 
+		// Remember the path
+		$this->data['path'] = $path;
+
 		// Redirect history
 		if ( ! $history )
 
@@ -184,8 +204,8 @@ class agent
 
 		// Configure the request
 		curl_setopt_array(
-			$curl = curl_init(), array(
-				CURLOPT_URL            => $path,
+			$this->data['curl'] = curl_init(), array(
+				CURLOPT_URL            => $this->data['path'],
 				CURLOPT_HEADER         => true,
 				CURLOPT_VERBOSE        => true,
 				CURLOPT_RETURNTRANSFER => true,
@@ -199,39 +219,37 @@ class agent
 		if ( ! is_null($this->data['post']) )
 		{
 			// Configure for a POST Request
-			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($this->data['curl'], CURLOPT_POST, true);
 
 			// Set the POST data
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $this->data['post']);
+			curl_setopt($this->data['curl'], CURLOPT_POSTFIELDS, $this->data['post']);
 		}
 
 		// Check if this is a DELETE request
 		if ( ! is_null($this->data['delete']) )
 		{
 			// Specialized custom DELETE headers added
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+			curl_setopt($this->data['curl'], CURLOPT_CUSTOMREQUEST, 'DELETE');
 		}
 
 		// Check if this is a PUT request
 		if ( ! is_null($this->data['put']) )
 		{
 			// Specialized custom PUT headers added
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+			curl_setopt($this->data['curl'], CURLOPT_CUSTOMREQUEST, 'PUT');
 
 			// Set the POST data
-			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($this->data['put']));
+			curl_setopt($this->data['curl'], CURLOPT_POSTFIELDS, http_build_query($this->data['put']));
 		}
 
-		// Cleanup previous data
-		$this->data['status']  = false;
-		$this->data['headers'] = null;
-		$this->data['body']    = null;
+		// Cleanup previous data and prepare for a new request
+		$this->resetter('pre', 'status');
 
 		// Send the request
-		$request = curl_exec($curl);
+		$this->data['request'] = curl_exec($this->data['curl']);
 
 		// Check for request errors
-		if( ! $request = curl_exec($curl) )
+		if( ! $this->data['request'] = curl_exec($this->data['curl']) )
 		{
 			// Zero out the status code
 			$this->data['status'] = false;
@@ -240,54 +258,101 @@ class agent
 			$this->data['headers'] = null;
 
 			// Return the CURL error
-			return $this->data['body'] = curl_error($curl);
+			return $this->data['body'] = curl_error($this->data['curl']);
 		}
 
-		// Start parsing the headers
-		foreach (explode("\r\n", substr($request, 0, strpos($request, "\r\n\r\n"))) as $headerline)
-		{
-			// Explode each headerline into KEY and VALUE
-			@list ($key, $value) = explode(': ', $headerline);
-
-			// Save the response headers
-			$this->data['headers'][$key] = $value;
-		}
-
-		// Standardize Header Location / Link for developers
-		$this->data['headers']['Path'] = $path;
-
-		// Set the header status code
-		$this->data['headers']['Status'] = key($this->data['headers']);
-
-		// Set the CURL status code
-		$this->data['code'] = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-		// Set the CURL body
-		$this->data['body'] = substr($request, curl_getinfo($curl, CURLINFO_HEADER_SIZE));
+		// Digest the CURL Request
+		$this->process_request();
 
 		// Detect redirects and record redirection history
-		if ( in_array($this->data['code'], [301, 302, 303, 307, 308]) )
+		if ( isset($this->data['code']) AND in_array($this->data['code'], [301, 302, 303, 307, 308]) )
 		{
 			// Add to the redirect history
 			$this->data['redirects'][] = array(
-				'Code'    => $this->data['code'],
-				'Status'  => $this->data['headers']['Status'],
-				'Path'    => $this->data['headers']['Path'],
+				'Status'  => $this->data['code'],
+				'Path'    => $this->data['path'],
+				'Protocol'=> $this->data['protocol'],
+				'Domain'  => $this->data['domain'],
 				'Headers' => $this->data['headers'],
-				'Body'    => $this->data['body']
+				'Body'    => $this->data['body'],
+				'Details' => $this->data['details']
 			);
 
 			// Reissue the request (keeping redirect history)
 			self::curl( $this->data['headers']['Location'], true );
 		}
 
-		// Perform cleanup
-		$this->data['put']    = null;
-		$this->data['post']   = null;
-		$this->data['delete'] = null;
+		// Cleanup data and prepare for a new request
+		$this->resetter('post');
 
-		// Return the model object for chaining
-		return $this;
+		// Return and set the status code
+		return $this->data['status'] = $this->data['code'];
+	}
+
+	/**
+	 * CURL Request Response Status
+	 *
+	 * @return 	string
+	 */
+	public function status()
+	{
+		// Get the full HTTP Status
+		return $this->data['code'];
+	}
+
+	/**
+	 * Returns the path used in the CURL Request
+	 *
+	 * @return 	string
+	 */
+	public function path()
+	{
+		// Get the path called
+		return $this->data['path'];
+	}
+
+	/**
+	 * Returns the protocol used in the CURL Request
+	 *
+	 * @return 	string
+	 */
+	public function protocol()
+	{
+		// Get protocol used
+		return $this->data['protocol'];
+	}
+
+	/**
+	 * Returns the domain used in the CURL Request
+	 *
+	 * @return 	string
+	 */
+	public function domain()
+	{
+		// Get the domain name
+		return $this->data['domain'];
+	}
+
+	/**
+	 * CURL Request Response Headers
+	 *
+	 * @return 	mixed
+	 */
+	public function headers()
+	{
+		// Get the HTTP Headers
+		return $this->data['headers'];
+	}
+
+	/**
+	 * CURL Request Response Body
+	 *
+	 * @return 	mixed
+	 */
+	public function body()
+	{
+		// Get the CURL Response Body
+		return $this->data['body'];
 	}
 
 	/**
@@ -302,47 +367,14 @@ class agent
 	}
 
 	/**
-	 * Request CURL HTTP Response Status Code
+	 * CURL Request Detailed Information
 	 *
-	 * @return 	int
+	 * @return 	mixed
 	 */
-	public function status_code()
-	{
-		// Get the HTTP Status Code
-		return $this->data['code'];
-	}
-
-	/**
-	 * Request CURL Response Status
-	 *
-	 * @return 	string
-	 */
-	public function status()
+	public function details()
 	{
 		// Get the full HTTP Status
-		return $this->data['headers']['Status'];
-	}
-
-	/**
-	 * Request CURL Response Headers
-	 *
-	 * @return 	mixed
-	 */
-	public function headers()
-	{
-		// Get the HTTP Headers
-		return $this->data['headers'];
-	}
-
-	/**
-	 * Request CURL Response Body
-	 *
-	 * @return 	mixed
-	 */
-	public function body()
-	{
-		// Get the CURL Response Body
-		return $this->data['body'];
+		return $this->data['details'];
 	}
 
 	/**
@@ -419,6 +451,89 @@ class agent
 	}
 
 	/**
+	 * Separates logic from curl() and handles most of the variable processing
+	 *
+	 * @param 	mixed
+	 * @param 	mixed
+	 * @return 	void
+	 */
+	private function process_request()
+	{
+		// Start parsing the headers
+		foreach (explode("\r\n", substr($this->data['request'], 0, strpos($this->data['request'], "\r\n\r\n"))) as $headerline)
+		{
+			// Explode each headerline into KEY and VALUE
+			@list ($key, $value) = explode(': ', $headerline);
+
+			// Save the response headers
+			$this->data['headers'][$key] = $value;
+		}
+
+		// Standardize Header Location / Link for developers
+		$this->data['headers']['Path'] = $this->data['path'];
+
+		// Set the header status code
+		$this->data['headers']['Status'] = key($this->data['headers']);
+
+		// Set the CURL status code
+		$this->data['code'] = curl_getinfo($this->data['curl'], CURLINFO_HTTP_CODE);
+
+		// Set the CURL body
+		$this->data['body'] = substr($this->data['request'], curl_getinfo($this->data['curl'], CURLINFO_HEADER_SIZE));
+
+		// Iterate through information from CURL Request details
+		foreach ($curlDetails = curl_getinfo($this->data['curl']) as $key => $cInfo)
+
+			// Detect duplicate information
+			if ( ! in_array($key, self::$ignore) )
+
+				// Standardize array key formatting and add to CURL Request details
+				$this->data['details'][ implode('-', array_map('ucwords',explode('_', str_replace('ip', 'IP', $key)))) ] = $cInfo;
+
+		// Parse the $path domain information
+		$domainArr = array_filter(explode('/', $curlDetails['url']));
+
+		// Set the protocol used
+		$this->data['protocol'] = strtolower(substr(array_shift($domainArr), 0, -1));
+
+		// Set the protocol used
+		$this->data['domain'] = array_shift($domainArr);
+	}
+
+	/**
+	 * Resets parameters to defaults for next CURL Request
+	 *
+	 * @param 	mixed
+	 * @return 	void
+	 */
+	private function resetter( ...$args )
+	{
+		// Params as keys
+		$args = array_flip($args);
+
+		// Parameter types for NULL resetting
+		foreach (['pre', 'post'] as $arg)
+
+			// Check which type
+			if ( isset($args[$arg]) )
+
+				// Iterate through these parameters
+				foreach ($this->param[$arg] as $key)
+
+					// Reset params
+					$this->data[$key] = null;
+
+		// Parameters for FALSE resetting
+		if ( isset($args['status']) )
+
+			// Iterate through these parameters
+			foreach ($this->param['status'] as $key)
+
+				// Reset params
+				$this->data[$key] = false;
+	}
+
+	/**
 	 * Method aliases and function wrappers for coders who like to use alternative
 	 * names for these methods. Slight performance impact when using method aliases.
 	 *
@@ -429,7 +544,8 @@ class agent
 	public function __call( $called, $args = array() )
 	{
 		$aliases = array(
-			'curl'	=> ['crawl', 'execute']
+			'curl'	 => ['execute'],
+			'status' => ['status_code', 'code', 'http_code']
 		);
 
 		// Iterate through methods
