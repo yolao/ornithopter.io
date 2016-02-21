f41f3ed00dd77bc7cae517b3cdd551da23714b68
Corey Olson <me@olscore.com>
2016-02-21 05:02:22 -0600

    Added methods to helpers\web ... seo(), slug(), stop_words(), is_stop_word()

--- application/helpers/web.php
+++ application/helpers/web.php
@@ -32,6 +32,11 @@
  * @method io::helpers('web')->querystring();
  * @method io::helpers('web')->index();
  * @method io::helpers('web')->current();
+ * @method io::helpers('web')->seo();
+ * @method io::helpers('web')->slug();
+ * @method io::helpers('web')->stop_words();
+ * @method io::helpers('web')->is_stop_word();
+ * @method io::helpers('web')->error_404();
  */
 namespace ornithopter\helpers;
 
@@ -96,6 +101,13 @@ class web
         504 => 'Gateway Timeout',
         505 => 'HTTP Version Not Supported',
     );
+
+    /**
+     * List of common SEO stop words.
+     *
+     * @var array
+     */
+    private static $seo = array('a','able','about','above','abroad','according','accordingly','across','actually','adj','after','afterwards','again','against','ago','ahead','ain\'t','all','allow','allows','almost','alone','along','alongside','already','also','although','always','am','amid','amidst','among','amongst','an','and','another','any','anybody','anyhow','anyone','anything','anyway','anyways','anywhere','apart','appear','appreciate','appropriate','are','aren\'t','around','as','a\'s','aside','ask','asking','associated','at','available','away','awfully','b','back','backward','backwards','be','became','because','become','becomes','becoming','been','before','beforehand','begin','behind','being','believe','below','beside','besides','best','better','between','beyond','both','brief','but','by','c','came','can','cannot','cant','can\'t','caption','cause','causes','certain','certainly','changes','clearly','c\'mon','co','co.','com','come','comes','concerning','consequently','consider','considering','contain','containing','contains','corresponding','could','couldn\'t','course','c\'s','currently','d','dare','daren\'t','definitely','described','despite','did','didn\'t','different','directly','do','does','doesn\'t','doing','done','don\'t','down','downwards','during','e','each','edu','eg','eight','eighty','either','else','elsewhere','end','ending','enough','entirely','especially','et','etc','even','ever','evermore','every','everybody','everyone','everything','everywhere','ex','exactly','example','except','f','fairly','far','farther','few','fewer','fifth','first','five','followed','following','follows','for','forever','former','formerly','forth','forward','found','four','from','further','furthermore','g','get','gets','getting','given','gives','go','goes','going','gone','got','gotten','greetings','h','had','hadn\'t','half','happens','hardly','has','hasn\'t','have','haven\'t','having','he','he\'d','he\'ll','hello','help','hence','her','here','hereafter','hereby','herein','here\'s','hereupon','hers','herself','he\'s','hi','him','himself','his','hither','hopefully','how','howbeit','however','hundred','i','i\'d','ie','if','ignored','i\'ll','i\'m','immediate','in','inasmuch','inc','inc.','indeed','indicate','indicated','indicates','inner','inside','insofar','instead','into','inward','is','isn\'t','it','it\'d','it\'ll','its','it\'s','itself','i\'ve','j','just','k','keep','keeps','kept','know','known','knows','l','last','lately','later','latter','latterly','least','less','lest','let','let\'s','like','liked','likely','likewise','little','look','looking','looks','low','lower','ltd','m','made','mainly','make','makes','many','may','maybe','mayn\'t','me','mean','meantime','meanwhile','merely','might','mightn\'t','mine','minus','miss','more','moreover','most','mostly','mr','mrs','much','must','mustn\'t','my','myself','n','name','namely','nd','near','nearly','necessary','need','needn\'t','needs','neither','never','neverf','neverless','nevertheless','new','next','nine','ninety','no','nobody','non','none','nonetheless','noone','no-one','nor','normally','not','nothing','notwithstanding','novel','now','nowhere','o','obviously','of','off','often','oh','ok','okay','old','on','once','one','ones','one\'s','only','onto','opposite','or','other','others','otherwise','ought','oughtn\'t','our','ours','ourselves','out','outside','over','overall','own','p','particular','particularly','past','per','perhaps','placed','please','plus','possible','presumably','probably','provided','provides','q','que','quite','qv','r','rather','rd','re','really','reasonably','recent','recently','regarding','regardless','regards','relatively','respectively','right','round','s','said','same','saw','say','saying','says','second','secondly','see','seeing','seem','seemed','seeming','seems','seen','self','selves','sensible','sent','serious','seriously','seven','several','shall','shan\'t','she','she\'d','she\'ll','she\'s','should','shouldn\'t','since','six','so','some','somebody','someday','somehow','someone','something','sometime','sometimes','somewhat','somewhere','soon','sorry','specified','specify','specifying','still','sub','such','sup','sure','t','take','taken','taking','tell','tends','th','than','thank','thanks','thanx','that','that\'ll','thats','that\'s','that\'ve','the','their','theirs','them','themselves','then','thence','there','thereafter','thereby','there\'d','therefore','therein','there\'ll','there\'re','theres','there\'s','thereupon','there\'ve','these','they','they\'d','they\'ll','they\'re','they\'ve','thing','things','think','third','thirty','this','thorough','thoroughly','those','though','three','through','throughout','thru','thus','till','to','together','too','took','toward','towards','tried','tries','truly','try','trying','t\'s','twice','two','u','un','under','underneath','undoing','unfortunately','unless','unlike','unlikely','until','unto','up','upon','upwards','us','use','used','useful','uses','using','usually','v','value','various','versus','very','via','viz','vs','w','want','wants','was','wasn\'t','way','we','we\'d','welcome','well','we\'ll','went','were','we\'re','weren\'t','we\'ve','what','whatever','what\'ll','what\'s','what\'ve','when','whence','whenever','where','whereafter','whereas','whereby','wherein','where\'s','whereupon','wherever','whether','which','whichever','while','whilst','whither','who','who\'d','whoever','whole','who\'ll','whom','whomever','who\'s','whose','why','will','willing','wish','with','within','without','wonder','won\'t','would','wouldn\'t','x','y','yes','yet','you','you\'d','you\'ll','your','you\'re','yours','yourself','yourselves','you\'ve','z','zero');
 
     /**
      * Ornithopter.io looks for an instance() method when loading a library.
@@ -406,6 +418,101 @@ class web
     {
         // Return the current request url
         return self::site().$_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * Returns a string or array with SEO stop words removed.
+     *
+     * @param mixed
+     *
+     * @return mixed
+     */
+    public static function seo($mixed)
+    {
+        // Detect strings
+        if (is_string($mixed)) {
+
+            // Separate the words and iterate through
+            foreach ($tArr = explode(' ', $mixed) as $key => $word) {
+
+                // Check against stop words
+                if (in_array($word, self::$seo)) {
+
+                    // Remove stop word
+                    unset($tArr[$key]);
+                }
+            }
+
+            // Cleaned string
+            return implode(' ', $tArr);
+        }
+
+        // Detect arrays
+        if (is_array($mixed)) {
+
+            // Iterate through items
+            foreach ($mixed as $key => $word) {
+
+                // Check against stop words
+                if (in_array($word, self::$seo)) {
+
+                    // Remove stop word
+                    unset($mixed[$key]);
+                }
+            }
+
+            // Cleaned array
+            return $mixed;
+        }
+
+        // Unexpected input
+        return false;
+    }
+
+    /**
+     * Returns a URL slug with or without SEO stop words removed.
+     *
+     * @param str
+     * @param boolean
+     *
+     * @return str
+     */
+    public static function slug($str, $seo = false)
+    {
+        // Normalize the string
+        $str = \io::helpers('str')->human(strtolower($str));
+
+        // Optional SEO stop word removal
+        if ($seo) {
+
+            // Get a purified string
+            $str = self::seo($str);
+        }
+
+        // Provide a title slug
+        return str_replace(' ', '-', $str);
+    }
+
+    /**
+     * Provides an array of SEO stop words.
+     *
+     * @return array
+     */
+    public static function stop_words()
+    {
+        // Returns the internal variable
+        return self::$seo;
+    }
+
+    /**
+     * Detects if string is a SEO stop word.
+     *
+     * @return array
+     */
+    public static function is_stop_word($str)
+    {
+        // Boolean response
+        return in_array($str, self::$seo);
     }
 
     /**
